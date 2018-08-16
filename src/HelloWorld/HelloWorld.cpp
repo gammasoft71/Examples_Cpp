f5cd9a30d007cad24cd59f96ea0ebefe1526de59
Gammasoft71 <gammasoft71@gmail.com>
2018-08-16 12:27:50 +0200

    init

---
+++ src/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  cout << "Hello, World!" << endl;
+}
+
+// This code produces the following output:
+//
+// Hello, World!
