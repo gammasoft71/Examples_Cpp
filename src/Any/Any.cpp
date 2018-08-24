25bf4d8e1c0a9f21cb27f8a930d1a5fc1f268a84
gammasoft71 <gammasoft71@gmail.com>
2018-08-24 13:04:35 +0200

    add any

--- src/Any/Any.cpp
+++ src/Any/Any.cpp
@@ -1,42 +1,42 @@
+#include <any>
 #include <iostream>
-#include <experimental/any>
+#include <string>
 
 using namespace std;
-using namespace std::experimental;
+using namespace std::string_literals;
 
 int main(int argc, char* argv[]) {
+  // Create an empty a
   any a;
-  cout << "a " << (a.empty() ? "hasn't" : "has") << " value" << endl;
-  //cout << "a = " << a << endl;
+  cout << "a " << (a.has_value() ? "has" : "hasn't") << " value" << endl;
+  cout << "a = " << endl;
   cout << endl;
   
   // Assign integer to a
   a = 42;
-  cout << "a = " << *any_cast<int32_t>(&a) << endl;
-  cout << "a " <<  (any_cast<int32_t>(&a) ? "is" : "isn't") << " int" << endl;
-  /*
-  Console::WriteLine("a {0} string", is<string>(a) ? "is" : "isn't");
-  int i = a;
-  Console::WriteLine("i = {0}", i);
+  cout << "a " << (any_cast<int>(&a) != nullptr ? "is" : "isn't") << " int" << endl;
+  cout << "a " << (any_cast<string>(&a) ? "is" : "isn't") << " string" << endl;
+  cout << "a = " << any_cast<int>(a) << endl;
+  int i = any_cast<int32_t>(a);
+  cout << "i = " << i << endl;
   cout << endl;
 
   // Assign string to a
-  a = "Hello, World!";
-  Console::WriteLine("a = {0}", a);
-  Console::WriteLine("a {0} int", is<int32>(a) ? "is" : "isn't");
-  Console::WriteLine("a {0} string", is<string>(a) ? "is" : "isn't");
-  string s = a;
-  Console::WriteLine("s = {0}", s);
+  a = "Hello, World!"s;
+  cout << "a " << (any_cast<int>(&a) != nullptr ? "is" : "isn't") << " int" << endl;
+  cout << "a " << (any_cast<string>(&a) ? "is" : "isn't") << " string" << endl;
+  cout << "a = " << any_cast<string>(a) << endl;
+  string s = any_cast<string>(a);
+  cout << "s = " << s << endl;
   cout << endl;
 
   // Cast any of type string into a int
   try {
-    int i2 = a;
-    Console::WriteLine("i2 = {0}", i2);
-  } catch (const InvalidCastException&) {
-    Console::WriteLine("a cannot cast in int");
+    int i2 = any_cast<int>(a);
+    cout << "i2 = " << i2 << endl;
+  } catch (const bad_any_cast&) {
+    cout << "'a' cannot be cast to int" << endl;
   }
-   */
 }
 
 // This code produces the following output:
