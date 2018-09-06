7bde46eeab1ddf9ccb21917d01d77df705cebf15
Gammasoft71 <gammasoft71@gmail.com>
2018-09-06 16:44:09 +0200

    Fix Any on macOS

--- src/Any/AnyM.cpp
+++ src/Any/AnyM.cpp
@@ -1,6 +1,7 @@
 #include <any>
 #include <iostream>
 #include <string>
+#include <utility>
 
 using namespace std;
 using namespace std::string_literals;
@@ -8,7 +9,7 @@ using namespace std::string_literals;
 int main(int argc, char* argv[]) {
   // Create an empty a
   any a;
-  cout << "a " << (a.has_value() ? "has" : "hasn't") << " value" << endl;
+  cout << "a " << (!a.has_value() ? "has" : "hasn't") << " value" << endl;
   cout << "a = " << endl;
   cout << endl;
   
@@ -16,8 +17,8 @@ int main(int argc, char* argv[]) {
   a = 42;
   cout << "a " << (any_cast<int>(&a) ? "is" : "isn't") << " int" << endl;
   cout << "a " << (any_cast<string>(&a) ? "is" : "isn't") << " string" << endl;
-  cout << "a = " << any_cast<int>(a) << endl;
-  int i = any_cast<int32_t>(a);
+  cout << "a = " << *any_cast<int>(&a) << endl;
+  int i = *any_cast<int32_t>(&a);
   cout << "i = " << i << endl;
   cout << endl;
 
@@ -25,16 +26,17 @@ int main(int argc, char* argv[]) {
   a = "Hello, World!"s;
   cout << "a " << (any_cast<int>(&a) ? "is" : "isn't") << " int" << endl;
   cout << "a " << (any_cast<string>(&a) ? "is" : "isn't") << " string" << endl;
-  cout << "a = " << any_cast<string>(a) << endl;
-  string s = any_cast<string>(a);
+  cout << "a = " << *any_cast<string>(&a) << endl;
+  string s = *any_cast<string>(&a);
   cout << "s = " << s << endl;
   cout << endl;
 
   // Cast any of type string into a int
   try {
-    int i2 = any_cast<int>(a);
+    if (!any_cast<int>(&a)) throw bad_cast();
+    int i2 = *any_cast<int>(&a);
     cout << "i2 = " << i2 << endl;
-  } catch (const bad_any_cast&) {
+  } catch (const bad_cast&) {
     cout << "'a' cannot be cast to int" << endl;
   }
 }
